804f1bb47f07589f8c4d3513c8ccc17e19467149
Christian Kleinhuis <ck@frontendsolutions.de>
2018-12-03 20:27:32 +0000

    ``q

--- test/test.sh
+++ test/test.sh
@@ -1,11 +1,33 @@
 #!/usr/bin/env bash
-../wraps/go
-../wraps/gradle
-../wraps/java
-../wraps/jq
-../wraps/maven
-../wraps/node
-../wraps/npm
-../wraps/python
-../wraps/robot
-../wraps/yarn
+set -x
+echo "Testing UFP Docker Wraps"
+
+
+
+echo "By Instantiating each"
+
+wrapCall()
+{
+    echo "Wrapping Call $@"
+	$@
+	RESULT=$?
+	if [ "${RESULT}" -ne 0 ]; then
+		echo "Shit happened"
+	exit 1
+	fi
+}
+
+
+wrapCall ../wraps/go version
+wrapCall ../wraps/gradle -version
+wrapCall ../wraps/java version
+wrapCall ../wraps/jq version
+wrapCall ../wraps/maven version
+wrapCall ../wraps/node version
+wrapCall ../wraps/npm
+wrapCall ../wraps/python
+wrapCall ../wraps/robot
+wrapCall ../wraps/yarn
+
+echo "Test Successful"
+
